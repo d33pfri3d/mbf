ffb6a3be7dde8b449f84c9b215fb5040d3efbcd4
Shaun Dunne <thedarkpoet@gmail.com>
2015-02-07 00:06:19 +0000

    Initial Commit

---
+++ Man's Best Friend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Man's Best Friend
+//
+//  Created by Shaun Dunne on 07/02/2015.
+//  Copyright (c) 2015 Shaun Dunne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Man's Best Friend/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Man's Best Friend
+//
+//  Created by Shaun Dunne on 07/02/2015.
+//  Copyright (c) 2015 Shaun Dunne. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
