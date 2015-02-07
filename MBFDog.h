a21640b20047bea24f28903c3ec39ee0895c99cc
Shaun Dunne <thedarkpoet@gmail.com>
2015-02-07 00:15:46 +0000

    initial commit

---
+++ MBFDog.h
@@ -0,0 +1,13 @@
+//
+//  MBFDog.h
+//  Man's Best Friend
+//
+//  Created by Shaun Dunne on 07/02/2015.
+//  Copyright (c) 2015 Shaun Dunne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBFDog : NSObject
+
+@end
---
+++ MBFDog.m
@@ -0,0 +1,13 @@
+//
+//  MBFDog.m
+//  Man's Best Friend
+//
+//  Created by Shaun Dunne on 07/02/2015.
+//  Copyright (c) 2015 Shaun Dunne. All rights reserved.
+//
+
+#import "MBFDog.h"
+
+@implementation MBFDog
+
+@end
